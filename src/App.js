186845b0cb4380f6cc727e089f9bada599ffc490
Jose Alejandro Olmedo <jose.olmedo@telusinternational.com>
2021-10-05 09:52:25 -0600

    first commit

--- src/App.js
+++ src/App.js
@@ -1,22 +1,36 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState, useEffect } from "react";
+import "./App.css";
+import axios from "axios";
 
 function App() {
+  const [bedTypesArr, setBedTypesArr] = useState([]);
+  useEffect(() => {
+    async function fetchMyAPI() {
+      let response = await axios.get(
+        "http://localhost:8080/v1/bedTypes/bedTypes2"
+      );
+      response = await response.data;
+      setBedTypesArr(response);
+    }
+    fetchMyAPI();
+    console.log(bedTypesArr.toString());
+  //eslint-disable-next-line
+  }, []);
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <ul>
+          {bedTypesArr.map((el, id) => {
+            return (
+              <li key={id}>
+                <h1>{el.name}</h1>
+                <h1>{el.size}</h1>
+              </li>
+            );
+          })}
+        </ul>
+        Learn React
       </header>
     </div>
   );
